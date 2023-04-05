fcd0d91d74f6cfcda1f77c578783e6096e555f92
Kaleb Garrett <kalebagarrett@gmail.com>
2023-04-04 21:25:53 -0400

    Removed unneeded code

--- Amiibo.Web/Services/AmiiboService.cs
+++ Amiibo.Web/Services/AmiiboService.cs
@@ -5,14 +5,8 @@ namespace Amiibo.Web.Services
 {
     public class AmiiboService
     {
-        public List<NintendoAmiibos> Amiibos { get; set; } = null;
-
         public async Task<List<NintendoAmiibos>> GetAll()
         {
-            if (Amiibos != null)
-            {
-                return Amiibos;
-            }
             using var client = new HttpClient();
             var result = await client.GetAsync("https://www.amiiboapi.com/api/amiibo/");
             if (result.IsSuccessStatusCode)
@@ -25,5 +19,3 @@ namespace Amiibo.Web.Services
         }
     }
 }
-//var dictionary = JsonSerializer.Deserialize<Dictionary<string, NintendoAmiibos>>(json);
-//return dictionary.Values.ToList();
